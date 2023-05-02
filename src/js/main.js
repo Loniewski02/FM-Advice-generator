e598e9cc3e67843b56a8a60b5fe3236ce6b9f4fb
Milosz <miloszloniewski02@gmail.com>
2023-05-02 11:45:53 +0200

    changed fetch to axios

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,20 @@ const sendBtnImg = document.querySelector('.app__dice img');
 const adviceNumber = document.querySelector('.app__title-number');
 const adviceText = document.querySelector('.app__text-advice');
 
-const getAdvice = () => {
-	fetch(`https://api.adviceslip.com/advice`)
-		.then(res => res.json())
-		.then(data => {
-			let adviceData = data.slip;
-			adviceNumber.textContent = adviceData.id;
-			adviceText.textContent = `${adviceData.advice}`;
-		});
-};
+let time = 1000;
+const URL = 'https://api.adviceslip.com/advice';
+
+async function getAdvice() {
+	try {
+		const response = await axios.get(URL);
+		let adviceData = response.data.slip;
+		adviceNumber.textContent = adviceData.id;
+		adviceText.textContent = `${adviceData.advice}`;
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
 
 const handleAnimation = () => {
 	sendBtn.classList.add('shake');
@@ -19,12 +24,12 @@ const handleAnimation = () => {
 	setTimeout(() => {
 		sendBtn.classList.remove('shake');
 		sendBtnImg.classList.remove('rotate');
-	}, 1000);
+	}, time);
 };
 
 sendBtn.addEventListener('click', () => {
 	handleAnimation();
 	setTimeout(() => {
 		getAdvice();
-	}, 1000);
+	}, time);
 });
